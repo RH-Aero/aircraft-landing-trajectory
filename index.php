29c950f929eccb9596fabc77d12d7703724f19e8
Alex Kharchenko <doom9936.1999@gmail.com>
2018-12-11 20:18:13 +0200

    add 6th motionpath

--- index.php
+++ index.php
@@ -753,8 +753,8 @@ ini_set('error_reporting', E_ALL);
       function placeMarker(dataTable) {
         var chart_li = this.getChartLayoutInterface();
         var chart_area = chart_li.getChartAreaBoundingBox();
-        document.querySelector('#chart_div_mp6').style.top = Math.floor(chart_li.getYLocation(dataTable.getValue(0, 0))) - 48 + "px";
-        document.querySelector('#chart_div_mp6').style.left = Math.floor(chart_li.getXLocation(dataTable.getValue(0, 0))) - 165 + "px";
+        document.querySelector('#chart_div_mp6').style.top = Math.floor(chart_li.getYLocation(dataTable.getValue(0, 0))) - 325 + "px";
+        document.querySelector('#chart_div_mp6').style.left = Math.floor(chart_li.getXLocation(dataTable.getValue(0, 0))) - 148 + "px";
       };
       google.visualization.events.addListener(chart, 'ready', placeMarker.bind(chart, chart_data));
       chart.draw(chart_data, chart_options);
